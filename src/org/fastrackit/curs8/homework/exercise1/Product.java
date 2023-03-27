8753714108187ed1385976275fd02967e8b76570
Maria Simon <maria_simon>
2023-03-27 13:17:33 +0300

    Corse 8 - Ex 1 Homework

---
+++ src/org/fastrackit/curs8/homework/exercise1/Product.java
@@ -0,0 +1,11 @@
+package org.fastrackit.curs8.homework.exercise1;
+
+public abstract class Product {
+    public abstract String name();
+
+    public abstract String description();
+
+    public abstract double price(double price);
+    public abstract int quantity(int quantity);
+
+}
