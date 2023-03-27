e05bee551765378302e04eb43e7b89e153b816b5
Maria Simon <maria_simon>
2023-03-27 23:12:59 +0300

    Corse 8 - Homework ex 2

---
+++ src/org/fastrackit/curs8/homework/exercise2/EmployeeMain.java
@@ -0,0 +1,24 @@
+package org.fastrackit.curs8.homework.exercise2;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee programmer = new Programmer("Michael", "Jordan", "8,Soporului Street, Bucharest");
+        programmer.getFirstName();
+        programmer.getLastName();
+        programmer.getAddress();
+        programmer.getBirthday();
+        programmer.dateOfEmployment();
+        ((Programmer) programmer).getPosition();
+        ((Programmer) programmer).language("Java expert");
+
+        Employee databaseAdmin = new DatabaseAdmin("Alin", "Matesan", "17,Calea Turzii Street, Bucharest");
+        databaseAdmin.getFirstName();
+        databaseAdmin.getLastName();
+        databaseAdmin.getAddress();
+        databaseAdmin.getBirthday();
+        databaseAdmin.dateOfEmployment();
+        ((DatabaseAdmin) databaseAdmin).setPosition();
+        ((DatabaseAdmin) databaseAdmin).dbTechnology("Database expert");
+
+    }
+}
