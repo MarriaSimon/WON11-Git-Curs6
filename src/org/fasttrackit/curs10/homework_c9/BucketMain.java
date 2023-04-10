512d675faece2aaff3417e532953ba3e2be5a234
Maria Simon <maria_simon>
2023-04-10 12:15:10 +0300

    course 10 ci

---
+++ src/org/fasttrackit/curs10/homework_c9/BucketMain.java
@@ -0,0 +1,9 @@
+package org.fasttrackit.curs10.homework_c9;
+
+public class BucketMain {
+    public static void main(String[] args) {
+        BucketFlowers bucket = new BucketFlowers();
+        bucket.add("banana");
+    }
+
+}
